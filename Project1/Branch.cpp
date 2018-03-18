af91b6c68d59fc8c3738cd98f583e218f54a81d5
jkpawlowski <pawlak0202@gmail.com>
2018-03-18 14:39:56 +0100

    znalezienie miejsca na element w drzewie

--- Project1/Branch.cpp
+++ Project1/Branch.cpp
@@ -4,9 +4,41 @@
 
 Branch::Branch()
 {
+		left = nullptr;
+		right = nullptr;
+		
 }
-
 
 Branch::~Branch()
 {
+	
 }
+
+void Branch::Add(const int &x)
+{
+	Branch *place= FindPlace( x);
+	place->value = x;
+}
+
+Branch * Branch::FindPlace(const int &x)
+{	
+	if (x < value) {
+		if (left != nullptr)
+			return left->FindPlace(x);
+		else {
+			left = new Branch(x);
+			return left;
+		}
+	}
+	
+	if (x > value) {
+		if (right != nullptr)
+			return right->FindPlace(x);
+		else {
+			right = new Branch(x);
+			return right;
+		}
+	}
+	
+	}
+
--- Project1/Branch.h
+++ Project1/Branch.h
@@ -1,8 +1,20 @@
 #pragma once
 class Branch
 {
+	int value;
+	int weight;
+	Branch *left;
+	Branch *right;
 public:
 	Branch();
+	Branch(const int &x) {
+		value = x;
+		Branch();
+	}
 	virtual ~Branch();
+	void Add(const int &x);
+	Branch* FindPlace(const int &x);
+
+
 };
 
